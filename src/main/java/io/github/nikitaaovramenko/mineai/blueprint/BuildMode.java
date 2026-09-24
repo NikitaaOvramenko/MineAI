c949037136d4e48f33ba9da92026698d13ed28f3
Nikita <ovramenko.nikitka@gmail.com>
2026-09-24 16:16:02 -0400

    commit

--- src/main/java/io/github/nikitaaovramenko/mineai/blueprint/BuildMode.java
+++
@@ -1,9 +0,0 @@
-package io.github.nikitaaovramenko.mineai.blueprint;
-
-// How a build treats what is already in its box.
-public enum BuildMode {
-    // Only fills empty space (air, water, grass): nothing the player built or mined is lost.
-    ADD,
-    // Makes the box match the blueprint, except for containers and other block entities, and unbreakable blocks.
-    REPLACE
-}
